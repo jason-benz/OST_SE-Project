40d87a059e7db9f4ef89fbcd0fde6ada09a3ca47
Adrian Locher <locher-adrian@hotmail.com>
2022-03-27 15:26:00 +0200

    Add userId to IdentityService

--- MediaHub/MediaHub.Data/Model/IIdentityService.cs
+++ MediaHub/MediaHub.Data/Model/IIdentityService.cs
@@ -2,6 +2,7 @@ namespace MediaHub.Data.Model;
 
 public class IIdentityService 
 {
+   public string? UserId { get; }
    public string? LoginName { get; }
    public bool IsAuthenticated { get; }
 
