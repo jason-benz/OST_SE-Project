0fb1ee1809a5721cfa52fece59ce472949f0c4cc
BK <benjamin.kern@ost.ch>
2022-03-31 22:09:53 +0200

    Initial draft of interfaces

---
+++ MediaHub/MediaHub.Data/Model/IMovieTMDB.cs
@@ -0,0 +1,13 @@
+﻿namespace MediaHub.Data.Model;
+
+public interface IMovieTMDB
+{
+    int Id { get; }
+    string Title { get; } // in /search is original_title
+    string PosterUrl { get; }
+    List<string> Genres { get; }
+    int Rating { get; }
+    string Overview { get; }
+    string Runtime { get; } // not in /search 
+    string ReleaseDate { get; }
+}
