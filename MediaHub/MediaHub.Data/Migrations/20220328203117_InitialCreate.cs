75f72162f16958cdbbd96e6b387251f63ee0b950
Jason Benz <j.benz@webbenz.ch>
2022-04-04 05:19:28 +0200

    Create user profile during email confirmation, moved db connection string

--- MediaHub/MediaHub.Data/Migrations/20220328203117_InitialCreate.cs
+++ MediaHub/MediaHub.Data/Migrations/20220328203117_InitialCreate.cs
@@ -8,7 +8,19 @@ namespace MediaHub.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.CreateTable(
+                name: "UserProfile",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 450, nullable: false),
+                    Username = table.Column<string>(maxLength: 50, nullable: false),
+                    Biography = table.Column<string>(maxLength: 256, nullable: true),
+                    ProfilePicture = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfile", x => x.Id);
+                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
