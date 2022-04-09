e4c7274ecae25797cf84b82e7c4fb442931ef2ce
Jason Benz <j.benz@webbenz.ch>
2022-04-09 15:19:01 +0200

    Added unit and integration tests

---
+++ MediaHub/MediaHub.Test/UserProfileTest/UserProfileDataManagerMock.cs
@@ -0,0 +1,33 @@
+﻿using MediaHub.Data.Model;
+using System;
+
+namespace MediaHub.Test.UserProfileTest
+{
+    internal class UserProfileDataManagerMock : IUserProfileDataManager
+    {
+        public UserProfile GetUserProfileById(string userId)
+        {
+            return new UserProfile(userId)
+            {
+                Username = "Mock test username",
+                Biography = "Mock test biography",
+                ProfilePicture = ProfilePicture.GetTestProfilePicture()
+            };
+        }
+
+        public UserProfile GetUserProfileByUsername(string username)
+        {
+            return new UserProfile(Guid.NewGuid().ToString())
+            {
+                Username = username,
+                Biography = "Mock test biography of method 'GetUserProfileByUsername'",
+                ProfilePicture = ProfilePicture.GetTestProfilePicture()
+            };
+        }
+
+        public void UpdateUserProfile(UserProfile userProfile)
+        {
+            // Method in mock is empty, because the single use case is to update the userprofile without return value
+        }
+    }
+}
---
+++ MediaHub/MediaHub.Test/UserProfileTest/UserProfileViewModelUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using MediaHub.Data.Model;
+using MediaHub.Data.ViewModel;
+using Xunit;
+
+namespace MediaHub.Test.UserProfileTest
+{
+    public class UserProfileViewModelUnitTest
+    {
+        private readonly IUserProfileViewModel _userProfileViewModel;
+
+        public UserProfileViewModelUnitTest()
+        {
+            _userProfileViewModel = new UserProfileViewModel(new UserProfileDataManagerMock());            
+        }
+
+        [Fact]
+        public void GetUserProfileById()
+        {
+            string userId = "123";
+            UserProfile userProfile = _userProfileViewModel.GetUserProfileById(userId);
+            Assert.Equal(userId, userProfile.UserId);
+        }
+
+        [Fact]
+        public void GetUserProfileByUsername()
+        {
+            string username = "MockUser";
+            UserProfile userProfile = _userProfileViewModel.GetUserProfileByUsername(username);
+            Assert.Equal(username, userProfile.Username);
+        }
+
+        [Fact]
+        public void UpdateUserProfileWithoutException()
+        {
+            var userProfile = new UserProfile("Test ID");
+            _userProfileViewModel.UpdateUserProfile(userProfile);
+            var exception = Record.Exception(() => new UserProfileViewModelUnitTest());
+            Assert.Null(exception);
+        }
+    }
+}
