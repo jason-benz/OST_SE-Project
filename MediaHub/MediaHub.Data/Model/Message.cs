273de8ef8378c00307f28f55774139b3bc01034e
BK <benjamin.kern@ost.ch>
2022-04-18 09:37:48 +0200

    Inital database setup for messages

---
+++ MediaHub/MediaHub.Data/Model/Message.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MediaHub.Data.Model;
+
+[Table("Message")]
+public class Message
+{
+    [Key]
+    [Column(Order=1)]
+    public UserProfile Sender { get; set; }
+    [Key]
+    [Column(Order=2)]
+    public UserProfile Receiver { get; set; }
+    [Key]
+    [Column(Order=3)]
+    public DateTime TimeSent { get; set; }
+    public DateTime TimeReceived { get; set; }
+    public string Content { get; set; }
+}
