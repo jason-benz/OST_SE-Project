3f115311aef50e1ac8002865aed8c3693d2f6573
BK <benjamin.kern@ost.ch>
2022-04-19 17:02:10 +0200

    Save work in progress

---
+++ MediaHub/MediaHub.Data/Model/IChatDataManager.cs
@@ -0,0 +1,9 @@
+﻿namespace MediaHub.Data.Model;
+
+public interface IChatDataManager
+{
+    public List<Message> GetAllMessages();
+
+    public void InsertMessage(Message message);
+    // TODO
+}
