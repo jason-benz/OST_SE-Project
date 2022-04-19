b9b84980d74fa6314e6094af278374c03c5c88c8
AL <adrian.locher@ost.ch>
2022-04-19 10:35:50 +0200

    renamed appsettings.json in data project

--- MediaHub/MediaHub.Data/Persistency/MediaHubDBContext.cs
+++ MediaHub/MediaHub.Data/Persistency/MediaHubDBContext.cs
@@ -23,7 +23,7 @@ public class MediaHubDBContext : DbContext
         {
             IConfigurationRoot configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile("appsettings_data.json", false, true)
                 .Build();
             
             optionsBuilder.UseSqlServer(configuration.GetConnectionString("DataConnection"))
