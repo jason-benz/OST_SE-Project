e9ee0509207a43867ebf85b232cf8ff6c14673ec
AL <adrian.locher@ost.ch>
2022-04-19 16:09:04 +0200

    add basic log functionality

---
+++ MediaHub/MediaHub/Services/SerilogService.cs
@@ -0,0 +1,27 @@
+using MediaHub.Data.Persistency;
+using Serilog.Core;
+
+namespace MediaHub.Services;
+
+public class SerilogService : ILogService
+{
+    private Logger _logger;
+    public SerilogService(Logger logger)
+    {
+        _logger = logger;
+    }
+    public void LogInformation(string message, ILogService.LogCategory category)
+    {
+        _logger.Information(FormatString(message, category));
+    }
+
+    public void LogException(string message, ILogService.LogCategory category, Exception exception)
+    {
+        _logger.Error(exception, FormatString(message, category));
+    }
+
+    private string FormatString(string message, ILogService.LogCategory category)
+    {
+        return $"{category.ToString()} -> {message}";
+    }
+}
