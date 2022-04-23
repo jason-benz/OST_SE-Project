f917b884463cf1e88e39fa1d0602252497c1c58b
Benjamin Kern <benjamin.kern98@gmail.com>
2022-04-23 17:06:50 +0200

    Add message to migrations

---
+++ MediaHub/MediaHub.Data/Migrations/20220423150511_Message.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediaHub.Data.Migrations
+{
+    public partial class Message : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Message",
+                columns: table => new
+                {
+                    TimeSent = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SenderUserId = table.Column<string>(type: "NVARCHAR(450)", nullable: false),
+                    ReceiverUserId = table.Column<string>(type: "NVARCHAR(450)", nullable: false),
+                    TimeReceived = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Message", x => x.TimeSent);
+                    table.ForeignKey(
+                        name: "FK_Message_UserProfile_ReceiverUserId",
+                        column: x => x.ReceiverUserId,
+                        principalTable: "UserProfile",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Message_UserProfile_SenderUserId",
+                        column: x => x.SenderUserId,
+                        principalTable: "UserProfile",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Message_ReceiverUserId",
+                table: "Message",
+                column: "ReceiverUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Message_SenderUserId",
+                table: "Message",
+                column: "SenderUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Message");
+        }
+    }
+}
