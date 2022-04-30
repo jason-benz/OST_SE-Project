723b3f1285be90c9b7585f92719e5a03aec8101f
BK <benjamin.kern@ost.ch>
2022-04-30 14:56:52 +0200

    Add prototype of contact list

--- MediaHub/MediaHub.Data/Persistency/ChatDataManager.cs
+++ MediaHub/MediaHub.Data/Persistency/ChatDataManager.cs
@@ -12,6 +12,14 @@ public class ChatDataManager : IChatDataManager
         return context.Messages.ToList();
     }
 
+    public List<Message> GetMessagesBetweenTwoUsers(string userId1, string userId2)
+    {
+        using MediaHubDBContext context = new();
+        return context.Messages.Where(m =>
+            (m.Receiver.UserId.Equals(userId1) && m.Sender.UserId.Equals(userId2)) ||
+            (m.Receiver.UserId.Equals(userId2) && m.Sender.UserId.Equals(userId1))).OrderBy(m => m.TimeSent).ToList();
+    }
+
     public void InsertMessage(Message m)
     {
         using MediaHubDBContext context = new();
--- MediaHub/MediaHub.Data/ViewModel/ChatViewModel.cs
+++ MediaHub/MediaHub.Data/ViewModel/ChatViewModel.cs
@@ -7,12 +7,36 @@ public class ChatViewModel : IChatViewModel
 {
     private readonly IChatDataManager _chatDataManager;
     private readonly IUserProfileDataManager _userProfileDataManager;
-
+    private UserProfile sender { get; set; }
+    private UserProfile receiver { get; set; }
+    
     public ChatViewModel(IChatDataManager chatDataManager, IUserProfileDataManager userProfileDataManager)
     {
         _chatDataManager = chatDataManager;
         _userProfileDataManager = userProfileDataManager;
 
+    }
+
+    public void SetSenderById(string userId)
+    {
+        receiver = _userProfileDataManager.GetUserProfileById(userId);
+    }
+    
+    public void SetReceiverById(string userId)
+    {
+        sender = _userProfileDataManager.GetUserProfileById(userId);
+    }
+
+    public List<UserProfile> GetAllContactUserProfiles()
+    {
+        // FIXME REMOVE THE SENDER FROM THIS LIST
+       // var wrappedSender = new List<UserProfile> {sender};
+        return _userProfileDataManager.GetAllUserProfiles();
+    }
+
+    public List<Message> GetAllMessagesForActiveChat()
+    {
+        return _chatDataManager.GetMessagesBetweenTwoUsers(receiver.UserId, sender.UserId);
     }
     
     // TODO Remove and replace with actual logic
@@ -23,10 +47,10 @@ public class ChatViewModel : IChatViewModel
     }
     
     // TODO Add parameters
-    public void InsertMessage(string senderProfileId, string receiverProfileId, string content)
+    public void InsertMessage(string content)
     {
-        UserProfile senderProfile = _userProfileDataManager.GetUserProfileById(senderProfileId);
-        UserProfile receiverProfile = _userProfileDataManager.GetUserProfileById(receiverProfileId);
+        UserProfile senderProfile = receiver;
+        UserProfile receiverProfile = sender;
         DateTime timeSent = DateTime.Now;
         Message m = new Message();
         m.Content = content;
