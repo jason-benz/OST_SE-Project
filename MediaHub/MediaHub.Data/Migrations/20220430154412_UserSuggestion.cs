f75f8dc0ddf2aed60ade8804ed6e71cf0cee1c5e
Jason Benz <j.benz@webbenz.ch>
2022-04-30 18:00:31 +0200

    Added UserSuggestion model and migration

---
+++ MediaHub/MediaHub.Data/Migrations/20220430154412_UserSuggestion.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediaHub.Data.Migrations
+{
+    public partial class UserSuggestion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserSuggestion",
+                columns: table => new
+                {
+                    UserId1 = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserId2 = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSuggestion", x => new { x.UserId1, x.UserId2 });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserSuggestion");
+        }
+    }
+}
