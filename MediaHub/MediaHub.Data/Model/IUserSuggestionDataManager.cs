053736d8da20c228bd5f873c67d3d8a63e5a81fb
JB <j.benz@webbenz.ch>
2022-05-01 23:40:30 +0200

    Implemented UserSuggestionEngine

---
+++ MediaHub/MediaHub.Data/Model/IUserSuggestionDataManager.cs
@@ -0,0 +1,13 @@
+﻿namespace MediaHub.Data.Model
+{
+    public interface IUserSuggestionDataManager
+    {
+        IEnumerable<UserSuggestion> GetSuggestedUsers(string userId);
+
+        IEnumerable<int> GetLikedMovieIdsByUserId(string userId);
+
+        IEnumerable<string> GetUserIdsToBeSuggested(IEnumerable<int> movieIds, IEnumerable<string> usersToIgnore);
+
+        void AddUserSuggestion(UserSuggestion userSuggestion);
+    }
+}
