7f5682723ef4988774342627f6fd2093a8ba4bc8
Jason Benz <j.benz@webbenz.ch>
2022-05-02 08:19:40 +0200

    Updated UserSuggestion with migration

--- MediaHub/MediaHub.Data/Migrations/20220501202159_UserSuggestion.cs
+++ MediaHub/MediaHub.Data/Migrations/20220501202159_UserSuggestion.cs
@@ -12,14 +12,31 @@ namespace MediaHub.Data.Migrations
                 name: "UserSuggestion",
                 columns: table => new
                 {
-                    UserId1 = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    UserId2 = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserId1 = table.Column<string>(type: "NVARCHAR(450)", nullable: false),
+                    UserId2 = table.Column<string>(type: "NVARCHAR(450)", nullable: false),
                     IgnoreSuggestion = table.Column<bool>(type: "bit", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_UserSuggestion", x => new { x.UserId1, x.UserId2 });
+                    table.ForeignKey(
+                        name: "FK_UserSuggestion_UserProfile_UserId1",
+                        column: x => x.UserId1,
+                        principalTable: "UserProfile",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_UserSuggestion_UserProfile_UserId2",
+                        column: x => x.UserId2,
+                        principalTable: "UserProfile",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
                 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSuggestion_UserId2",
+                table: "UserSuggestion",
+                column: "UserId2");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
