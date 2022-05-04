b79dbdf6b921eda839ebe1b279cf3a256fac1c8a
Jason Benz <j.benz@webbenz.ch>
2022-05-04 09:16:55 +0200

    Added FeedItem with migrations

---
+++ MediaHub/MediaHub.Data/FeedModule/Model/FeedItem.cs
@@ -0,0 +1,25 @@
+﻿using MediaHub.Data.ProfileModule.Model;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MediaHub.Data.FeedModule.Model
+{
+    [Table("FeedItem")]
+    public class FeedItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public UserProfile UserProfile { get; set; }
+
+        public Tables ChangedTable { get; set; }
+
+        public string OldValue { get; set; }
+
+        public string NewValue { get; set; }
+    }
+}
