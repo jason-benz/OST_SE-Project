dad2e5aeda0002a2d9d92cdd3a8b68d71d441ec6
JB <j.benz@webbenz.ch>
2022-05-04 09:44:11 +0200

    Added empty feed view

---
+++ MediaHub/MediaHub.Data/FeedModule/ViewModel/FeedViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MediaHub.Data.FeedModule.Model;
+
+namespace MediaHub.Data.FeedModule.ViewModel
+{
+    public class FeedViewModel : IFeedViewModel
+    {
+        private readonly IFeedDataManager _feedDataManager;
+
+        public FeedViewModel(IFeedDataManager feedDataManager)
+        {
+            _feedDataManager = feedDataManager;
+        }
+    }
+}
