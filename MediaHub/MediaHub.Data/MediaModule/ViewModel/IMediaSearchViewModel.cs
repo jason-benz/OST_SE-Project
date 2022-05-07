216b2ebbbb1548879f7f1c5864ace11db4d12962
AL <adrian.locher@ost.ch>
2022-05-07 14:18:14 +0200

    update tests, add script for report generation

--- MediaHub/MediaHub.Data/MediaModule/ViewModel/IMediaSearchViewModel.cs
+++ MediaHub/MediaHub.Data/MediaModule/ViewModel/IMediaSearchViewModel.cs
@@ -5,6 +5,4 @@ namespace MediaHub.Data.MediaModule.ViewModel;
 public interface IMediaSearchViewModel
 {
     public Task<Movie> GetMovieAsync(int id);
-
-    public Task<List<Movie>> GetMoviesAsync(string searchString);
 }
