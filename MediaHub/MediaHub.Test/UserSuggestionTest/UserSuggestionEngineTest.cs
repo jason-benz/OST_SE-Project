5be7ac8010dfd9154ccaf171ae04ff891b803e40
Jason Benz <j.benz@webbenz.ch>
2022-05-15 17:55:27 +0200

    Added UserSuggestionEngine UnitTest

---
+++ MediaHub/MediaHub.Test/UserSuggestionTest/UserSuggestionEngineTest.cs
@@ -0,0 +1,27 @@
+﻿using MediaHub.Data.UserSuggestionModule.Model;
+using System.Threading;
+using Xunit;
+
+namespace MediaHub.Test.UserSuggestionTest
+{
+    public class UserSuggestionEngineTest
+    {
+        private readonly IUserSuggestionEngine _userSuggestionEngine;
+        private readonly UserSuggestionDataManagerMock _userSuggestionDataManager;
+
+        public UserSuggestionEngineTest()
+        {
+            _userSuggestionDataManager = new UserSuggestionDataManagerMock();
+            _userSuggestionEngine = new UserSuggestionEngine(_userSuggestionDataManager);
+        }
+
+        [Fact, Trait("Category", "Unit")]
+        public void StartUserSuggestionEngine()
+        {
+            string userId = "MockId-1";
+            _userSuggestionEngine.StartUserSuggestionEngine(userId);
+            Thread.Sleep(250);
+            Assert.Equal(userId, _userSuggestionDataManager.UserId);
+        }
+    }
+}
