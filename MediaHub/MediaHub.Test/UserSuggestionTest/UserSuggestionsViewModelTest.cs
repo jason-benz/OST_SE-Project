4faa22973310957ddf261549b4870bb5831de705
JB <j.benz@webbenz.ch>
2022-05-15 17:04:10 +0200

    Added UserSuggestionsViewModelTest

---
+++ MediaHub/MediaHub.Test/UserSuggestionTest/UserSuggestionsViewModelTest.cs
@@ -0,0 +1,35 @@
+﻿using MediaHub.Data.UserSuggestionModule.Model;
+using MediaHub.Data.UserSuggestionModule.ViewModel;
+using System.Linq;
+using Xunit;
+
+namespace MediaHub.Test.UserSuggestionTest
+{
+    public class UserSuggestionsViewModelTest
+    {
+        private readonly IUserSuggestionsViewModel _userSuggestionsViewModel;
+
+        public UserSuggestionsViewModelTest()
+        {
+            _userSuggestionsViewModel = new UserSuggestionsViewModel(new UserSuggestionDataManagerMock());
+        }
+
+        [Theory, Trait("Category", "Unit")]
+        [InlineData("MockId-1", false, 0)]
+        [InlineData("MockId-2", false, 1)]
+        [InlineData("MockId-2", true, 2)]
+        public void GetUserSuggestions(string userId, bool loadIgnoredSuggestions, int expectedResult)
+        {
+            var suggestedUsers = _userSuggestionsViewModel.GetUserSuggestions(userId, loadIgnoredSuggestions);
+            Assert.Equal(expectedResult, suggestedUsers.Count());
+        }
+
+        [Fact, Trait("Category", "Unit")]
+        public void IgnoreUserSuggestion()
+        {
+            var userSuggestion = new UserSuggestion();
+            var exception = Record.Exception(() => _userSuggestionsViewModel.IgnoreUserSuggestion(userSuggestion));
+            Assert.Null(exception);
+        }
+    }
+}
