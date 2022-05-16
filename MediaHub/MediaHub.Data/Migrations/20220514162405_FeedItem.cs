eb9a437c999f836a22f6b0cd31b09b8ffc38f7f2
JB <j.benz@webbenz.ch>
2022-05-16 16:23:36 +0200

    Added code coverage exclude

--- MediaHub/MediaHub.Data/Migrations/20220514162405_FeedItem.cs
+++ MediaHub/MediaHub.Data/Migrations/20220514162405_FeedItem.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace MediaHub.Data.Migrations
 {
+    [ExcludeFromCodeCoverage]
     public partial class FeedItem : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
