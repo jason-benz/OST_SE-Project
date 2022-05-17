e5a89dd766ae9c39089f031f5b1abc1488173c25
DE <dominik.ehrle@ost.ch>
2022-05-17 11:44:03 +0200

    finalized mediaComment-function; comment-text expandable, no text-limit, new migration

--- MediaHub/MediaHub.Data/MediaModule/Model/MediaComment.cs
+++ MediaHub/MediaHub.Data/MediaModule/Model/MediaComment.cs
@@ -13,18 +13,5 @@ public class MediaComment
     [Column("UserId", TypeName = "NVARCHAR(450)")]
     public string UserId { get; set; }
     public DateTime Created { get; set; }
-    private string _commentText = string.Empty;
-    private const int CommentMaxChar = 200;
-    public string CommentText
-    {
-        get => _commentText;
-        set
-        {
-            if (value.Length > CommentMaxChar)
-            {
-                throw new ArgumentOutOfRangeException(value, "Comment has to be less than 200 characters");
-            }
-            _commentText = value;
-        }
-    }
+    public string CommentText { get; set; }
 }
