65e1b4622df545ecb4fd9b669dc5c1ac6e2d7a96
BK <benjamin.kern@ost.ch>
2022-05-19 15:05:09 +0200

    chathub must authorize, everything else works

--- MediaHub/MediaHub.Data/MessagingModule/Model/ChatHub.cs
+++ MediaHub/MediaHub.Data/MessagingModule/Model/ChatHub.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 
 namespace MediaHub.Data.MessagingModule.Model;
 
+[Authorize]
 public class ChatHub : Hub<IChatClient>
 {
     public async Task SendMessage(Message message)
     {
-        await Clients.All.ReceiveMessage(message);
-        //await Clients.User(message.Receiver.UserId).ReceiveMessage(message);
+        //await Clients.All.ReceiveMessage(message);
+        await Clients.User(message.Receiver.UserId).ReceiveMessage(message);
         //await Clients.User(message.Receiver.UserId).SendAsync("ReceiveMessage", message);
     }
 }
