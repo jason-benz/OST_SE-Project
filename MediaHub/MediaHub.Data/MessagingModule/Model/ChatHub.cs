e8cf7c28468580ddc8d433a249a811c7da18a53d
Benjamin Kern <benjamin.kern98@gmail.com>
2022-05-19 18:54:27 +0200

    Workaround for failing authentication in chathub (should be inherited)--> send messages to everyone, and they filter which messages to display

--- MediaHub/MediaHub.Data/MessagingModule/Model/ChatHub.cs
+++ MediaHub/MediaHub.Data/MessagingModule/Model/ChatHub.cs
@@ -3,14 +3,13 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace MediaHub.Data.MessagingModule.Model;
 
-[Authorize]
+//[Authorize]
 public class ChatHub : Hub<IChatClient>
 {
     public async Task SendMessage(Message message)
     {
-        //await Clients.All.ReceiveMessage(message);
-        await Clients.User(message.Receiver.UserId).ReceiveMessage(message);
-        //await Clients.User(message.Receiver.UserId).SendAsync("ReceiveMessage", message);
+        await Clients.All.ReceiveMessage(message);
+        // await Clients.User(message.Receiver.UserId).ReceiveMessage(message);
     }
 }
 
