b2b650c222399709bbfa6ac045e61b6081a06af7
Benjamin Kern <benjamin.kern98@gmail.com>
2022-05-19 19:03:57 +0200

    Code cleanup

--- MediaHub/MediaHub.Data/MessagingModule/Model/ChatHub.cs
+++ MediaHub/MediaHub.Data/MessagingModule/Model/ChatHub.cs
@@ -3,13 +3,11 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace MediaHub.Data.MessagingModule.Model;
 
-//[Authorize]
 public class ChatHub : Hub<IChatClient>
 {
     public async Task SendMessage(Message message)
     {
         await Clients.All.ReceiveMessage(message);
-        // await Clients.User(message.Receiver.UserId).ReceiveMessage(message);
     }
 }
 
