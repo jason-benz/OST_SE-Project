7bbba47ec485bc55228f1f47983515107ddd4973
Jason Benz <j.benz@webbenz.ch>
2022-05-22 18:43:26 +0200

    Updated UserSuggestion tests

--- MediaHub/MediaHub.Test/UserSuggestionTest/UserSuggestionsViewModelTest.cs
+++ MediaHub/MediaHub.Test/UserSuggestionTest/UserSuggestionsViewModelTest.cs
@@ -1,6 +1,9 @@
-﻿using MediaHub.Data.UserSuggestionModule.Model;
+﻿using MediaHub.Data.ContactsModule.Model;
+using MediaHub.Data.PersistencyLayer;
+using MediaHub.Data.UserSuggestionModule.Model;
 using MediaHub.Data.UserSuggestionModule.ViewModel;
 using MediaHub.Test.ContactTest;
+using MediaHub.Test.LogTests;
 using System.Linq;
 using Xunit;
 
@@ -13,23 +16,38 @@ namespace MediaHub.Test.UserSuggestionTest
         public UserSuggestionsViewModelTest()
         {
             _userSuggestionsViewModel = new UserSuggestionsViewModel(new UserSuggestionDataManagerMock(), new ContactDataManagerMock());
+            ILogService.Singleton = new LogServiceMock();
         }
 
         [Theory, Trait("Category", "Unit")]
         [InlineData("MockId-1", false, 0)]
         [InlineData("MockId-2", false, 1)]
         [InlineData("MockId-2", true, 2)]
+        [InlineData("MockId-3", true, 0)]
+        [InlineData("MockId-3", false, 0)]
         public void GetUserSuggestions(string userId, bool loadIgnoredSuggestions, int expectedResult)
         {
             var suggestedUsers = _userSuggestionsViewModel.GetUserSuggestions(userId, loadIgnoredSuggestions);
             Assert.Equal(expectedResult, suggestedUsers.Count());
         }
 
-        [Fact, Trait("Category", "Unit")]
-        public void IgnoreUserSuggestion()
+        [Theory, Trait("Category", "Unit")]
+        [InlineData("MockId-1")]
+        [InlineData("MockId-2")]
+        public void IgnoreUserSuggestion(string userId)
         {
-            var userSuggestion = new UserSuggestion();
+            var userSuggestion = new UserSuggestion { UserId1 = userId };
             var exception = Record.Exception(() => _userSuggestionsViewModel.IgnoreUserSuggestion(userSuggestion));
+            Assert.Null(exception);
+        }
+
+        [Theory, Trait("Category", "Unit")]
+        [InlineData("MockId-1", "MockId-1-Contact")]
+        [InlineData("MockId-2", "MockId-2-Contact")]
+        [InlineData("MockId-3", "MockId-3-Contact")]
+        public void AddToContact(string userId, string contactId)
+        {
+            var exception = Record.Exception(() => _userSuggestionsViewModel.AddToContact(userId, contactId));
             Assert.Null(exception);
         }
     }
